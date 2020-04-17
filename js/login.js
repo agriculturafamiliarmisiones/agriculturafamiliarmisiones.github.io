39d4ec09aca12b9a65a60aa2edc5ef18b5e46105
agriculturafamiliarmisiones <agriculturafamiliarmisiones@gmail.com>
2020-04-17 11:36:11 -0300

    version 1

---
+++ js/login.js
@@ -0,0 +1,84 @@
+/**=============================================================================
+ *
+ *	Filename:  function.ajax.js
+ *	
+ *	(c)Autor: Arkos Noem Arenom
+ *	
+ *	Description: Ajax para hacer las consultas
+ *	
+ *	Licence: GPL|LGPL
+ *	
+ *===========================================================================**/
+
+$(document).ready(function(){
+
+  var timeSlide = 300;
+  $('#userid').focus();
+  
+  $('#timer').hide(0);
+  $('#timer').css('display','none');
+
+  $('#login_userbttn').click(function(){
+  	
+    $('#timer').fadeIn(200);
+    $('.box-info, .box-success, .box-alert, .box-error').slideUp(timeSlide);
+    setTimeout(function(){
+	    if ( $('#userid').val() != "" && $('#password').val() != "" ){
+
+		    $.ajax({
+			    type: 'POST',
+			    url: 'index.php?ctrl=Login&accion=Verificar',
+			    data: 'login_name=' + $('#userid').val() + '&login_pass=' + $('#password').val(),
+			    dataType : 'json',
+			    success:function(data){
+
+				    if ( !data.error ){
+					    $('#alertBoxes').html('<div class="box-success"></div>');
+					    $('.box-success').hide(0).html('Espera un momento&#133;');
+					    $('.box-success').slideDown(timeSlide);
+					    setTimeout(function(){
+						    window.location.href = data.url;
+					    },(timeSlide + 100));
+				    }
+				    else{
+					    $('#alertBoxes').html('<div class="box-error"></div>');
+					    //$('.box-error').hide(0).html('Lo sentimos, pero los datos son incorrectos: ' + data.error);
+					    $('.box-error').hide(0).html('Lo sentimos, pero los datos son incorrectos.');
+					    $('.box-error').slideDown(timeSlide);
+				    }
+				    $('#timer').fadeOut(200);
+			    },
+			    error:function(){
+				    $('#timer').fadeOut(200);
+				    $('#alertBoxes').html('<div class="box-error"></div>');
+				    $('.box-error').hide(0).html('Ha ocurrido un error durante la ejecuci&oacute;n');
+				    $('.box-error').slideDown(timeSlide);
+			    }
+		    });
+
+	    }
+	    else{
+		    $('#alertBoxes').html('<div class="box-error"></div>');
+		    $('.box-error').hide(0).html('Los campos estan vacios');
+		    $('.box-error').slideDown(timeSlide);
+		    $('#timer').fadeOut(200);
+	    }
+    },timeSlide);
+
+    return false;
+
+  });
+
+
+
+  $('#sessionKiller').click(function(){
+	  $('#timer').fadeIn(200);
+	  $('#alertBoxes').html('<div class="box-success"></div>');
+	  $('.box-success').hide(0).html('Espera un momento&#133;');
+	  $('.box-success').slideDown(timeSlide);
+	  setTimeout(function(){
+		  window.location.href = "index.php?controlador=login&accion=deslogearse";
+	  },300);
+  });
+
+});
